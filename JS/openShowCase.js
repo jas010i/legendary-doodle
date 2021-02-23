b1dc8a0376f302d0ed9c94f28b894a6efd0c9424
jas010i <63612124+jas010i@users.noreply.github.com>
2021-02-23 00:06:39 -0600

    k,

---
+++ JS/openShowCase.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+function openCT() {
+    location.replace("https://jas010i.github.io/charmy_treatss/index.html")
+}
+
+function openIP() {
+    location.replace("https://jas010i.github.io/iridescXnt-portfolio/")
+}
+
+function openSACH() {
+    location.replace("http://jas010i.github.io/SACH")
+}
+
+function openHEAT() {
+    location.replace("https://jas010i.github.io/APP-SACARHUB/")
+}
+
+function openYP() {
+    location.replace("https://jas010i.github.io/legendary-doodle/")
+}
